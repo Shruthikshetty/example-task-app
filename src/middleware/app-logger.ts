9ec5492ae00bad6126d019976cdc95b675fd7ceb
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-14 00:35:27 +0530

    add looger file

--- src/middleware/app-logger.ts
+++ src/middleware/app-logger.ts
@@ -1,11 +1,42 @@
 import { pinoLogger } from "hono-pino";
-import PinoPretty from "pino-pretty";
 
 // config for pino logger
 export const appLogger = pinoLogger({
-  pino: PinoPretty({
-    colorize: true,
-  }),
+  pino: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+        translateTime: "SYS:standard", // clearer timestamp
+      },
+    },
+    formatters: {
+      level: (label) => {
+        return {
+          level: label,
+        };
+      },
+      log: (object: any) => {
+        // if success (status < 400), reduce log verbsity
+        if (object.res && object.res.status < 400) {
+          // keep only method and url from req
+          if (object.req) {
+            object.req = {
+              method: object.req.method,
+              url: object.req.url,
+            };
+          }
+          // keep only status from res
+          if (object.res) {
+            object.res = {
+              status: object.res.status,
+            };
+          }
+        }
+        return object;
+      },
+    },
+  },
   http: {
     reqId: () => crypto.randomUUID(),
   },
