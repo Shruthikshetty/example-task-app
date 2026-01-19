d1ce40f13aea69293ac43fbe8f03fe532c070595
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-19 21:39:09 +0530

    code clean up

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,10 @@
-import { OpenAPIHono } from "@hono/zod-openapi";
+import createApp from "./lib/create-app.js";
 
-import type { AppBindings } from "./types.js";
-
-import { appLogger } from "./middleware/app-logger.js";
-
-// Zod OpenAPI Hono is an extended Hono class that supports OpenAPI. With it, you can validate values and types using Zod and generate OpenAPI Swagger documentation.
-const app = new OpenAPIHono<AppBindings>();
+const app = createApp();
 
 // base route
 app.get("/", (c) => {
   return c.text("example task app");
-});
-
-// app logger
-app.use(appLogger);
-
-// example route
-app.get("/example", (c) => {
-  // c.var.logger.info("example route");
-  return c.json({ message: "example route" });
-});
-
-app.get("/example-error", () => {
-  throw new Error("example error");
-});
-
-// custom error handler
-app.onError((err, c) => {
-  return c.json({
-    message: err?.message ?? "Something went wrong",
-    statusCode: 500,
-  });
-});
-
-// handle not found
-app.notFound((c) => {
-  return c.json({
-    message: "route not found",
-    statusCode: 404,
-  });
 });
 
 export default app;
---
+++ src/lib/create-app.ts
@@ -0,0 +1,40 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { serveEmojiFavicon } from "stoker/middlewares";
+
+import type { AppBindings } from "@/types.js";
+
+import { appLogger } from "@/middleware/app-logger.js";
+
+// a function that creates the app with all the middlewares
+const createApp = () => {
+  // Zod OpenAPI Hono is an extended Hono class that supports OpenAPI. With it, you can validate values and types using Zod and generate OpenAPI Swagger documentation.
+  const app = new OpenAPIHono<AppBindings>({
+    strict: false,
+  });
+
+  // serve a favicon
+  app.use(serveEmojiFavicon("📝"));
+
+  // app logger
+  app.use(appLogger);
+
+  // custom error handler
+  app.onError((err, c) => {
+    return c.json({
+      message: err?.message ?? "Something went wrong",
+      statusCode: 500,
+    });
+  });
+
+  // handle not found
+  app.notFound((c) => {
+    return c.json({
+      message: "route not found",
+      statusCode: 404,
+    });
+  });
+
+  return app;
+};
+
+export default createApp;
