f0379aa435a0db593392a4e48f1a5777faf31a15
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-19 10:53:27 +0530

    add env validator

--- env.ts
+++ env.ts
@@ -7,14 +7,26 @@ expand(config());
 
 const EnvSchema = z.object({
   PORT: z.coerce.number().default(3000),
-  LOG_LEVEL: z.string().default("debug"),
-  NODE_ENV: z
+  NODE_ENV: z.string().default("development"),
+  LOG_LEVEL: z
     .enum(["fatal", "error", "warn", "info", "debug", "trace"])
     .default("debug"),
 });
 
-// eslint-disable-next-line node/no-process-env
-const env = EnvSchema.parse(process.env);
+type ENV = z.infer<typeof EnvSchema>;
 
+let tempEnv: ENV;
+
+try {
+  // eslint-disable-next-line node/no-process-env
+  tempEnv = EnvSchema.parse(process.env);
+} catch (err) {
+  const error = err as z.ZodError;
+  console.error("Invalid environment variables: ", z.flattenError(error));
+  //exist the app in case of error
+  process.exit(1);
+}
+
+const env = tempEnv;
 //export type save env
 export default env;
