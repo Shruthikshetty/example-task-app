bd9ac9fcd6f54474e6f62a5492fdf05b94ac5b68
shruthik shetty <shrutikshetty14@gmail.com>
2026-01-23 13:14:09 +0530

    update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,17 +1,15 @@
-import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 // task table schema
 export const tasks = sqliteTable("tasks", {
   id: integer({ mode: "number" }).notNull().primaryKey({ autoIncrement: true }),
-  done: integer({ mode: "boolean" }).notNull(),
+  done: integer({ mode: "boolean" }).notNull().default(false),
   name: text().notNull(),
-  createdAt: integer("created_at", { mode: "timestamp" })
-    .notNull()
-    .default(sql`(unixepoch())`),
-
+  createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(
+    () => new Date(),
+  ),
   updatedAt: integer("updated_at", { mode: "timestamp" })
     .notNull()
-    .default(sql`(unixepoch())`)
+    .$defaultFn(() => new Date())
     .$onUpdate(() => new Date()),
 });
